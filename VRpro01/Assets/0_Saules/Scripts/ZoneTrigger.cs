942f1f3edb7b95a48abff026bc74658c27cb12a6
saulesmugis <saulle1033@gmail.com>
2023-04-07 03:57:14 +0300

    Imported button

--- VRpro01/Assets/0_Saules/Scripts/ZoneTrigger.cs
+++ VRpro01/Assets/0_Saules/Scripts/ZoneTrigger.cs
@@ -6,14 +6,14 @@ public class ZoneTrigger : MonoBehaviour
 {
     public GameObject objectToSpawn;
     public Transform spawnLocation;
-    private bool hasSpawned = false;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if ( other.gameObject.layer == LayerMask.NameToLayer("Icon"))
         {
             Instantiate(objectToSpawn, spawnLocation.position, spawnLocation.rotation);
-            hasSpawned = true;
+      
             gameObject.SetActive(false);
         }
     }
